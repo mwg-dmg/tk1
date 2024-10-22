804b1c38617bab6514f84668948c91ec005a5b3d
mwg-dmg <156708696+mwg-dmg@users.noreply.github.com>
2024-10-22 20:33:17 +0530

    Update 1_Treks.py

--- pages/1_Treks.py
+++ pages/1_Treks.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 st.set_page_config(page_title="MWG",page_icon="&",layout="centered",initial_sidebar_state="auto",menu_items=None)
 if "page" not in st.session_state:
-    st.session_state.page = 0
+    st.session_state.page = 1
 
 
 placeholder = st.empty()
