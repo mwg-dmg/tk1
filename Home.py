ddf5d6a2bd247db97ff196bc37f02c1611a330cd
mwg-dmg <156708696+mwg-dmg@users.noreply.github.com>
2024-10-22 20:31:53 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="MWG",page_icon="&",layout="centered",initial_side
 
 
 if "page" not in st.session_state:
-    st.session_state.page = 0
+    st.session_state.page = 1
 
 
 
