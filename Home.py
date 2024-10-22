a50db836efc607cdc10916d5b9d35eeaa5c894f3
mwg-dmg <156708696+mwg-dmg@users.noreply.github.com>
2024-10-22 14:20:16 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -35,30 +35,38 @@ if st.session_state.page == 0:
         login=st.text_input('Login Name',key="one",placeholder="Login Name")
         password=st.text_input('Password',key="two",placeholder="Password")
         st.button("Submit",key=None,on_click=lgin)
-
+        st.write("Key in the login name and password, do not use auto words")
 elif st.session_state.page > 0:
     
     with placeholder.container():
-              
+            st.session_state.page = 1   
             st.image("x1.jpg")
-            st.text(
+            st.write(
                     """
-                        Trekking is an exhilarating experience that offers breath taking views and
-                        sense of  accomplishment. We  have done lot of  trekking together. It is a
-                        great  bonding  experience. We  enjoyed  all  the treks and it  helped  in
-                        relaxing our mind  every time. The energy continued to be with us for many
-                        days after  the trek. Trekking leaves  an inedible mark on  your heart and
-                        soul.These treks have created  life long memories. But now  these memories
-                        are fading away.So let us  cherish these moments of time we spent together
-                        by compiling the photos and videos of our treks. 
-                        So go ahead!
-                        And... yes.... if  you have any  exclusive photo or video of our
-                        trek please do not forget to send it to MWG or AAG on Whatsapp.
+                        Trekking is an exhilarating experience that offers breath taking
+                        views and sense of accomplishment. We have done lot of trekking
+                        together. It is a great bonding experience. We enjoyed all the
+                        treks and it helped in relaxing our mind every time.
+                        The energy continued to be with us for many days after the trek.
+                        Trekking leaves an inedible mark on your heart and soul.
+                        These treks have created life long memories.
+                        But now these memories are fading away.
+                        So let us cherish these moments of time we spent together by
+                        compiling the photos and videos of our treks. 
+                        So go ahead!                        
+                        
+                    """
+                    )
+            st.write("""
+                        And... yes... if you have any exclusive
+                        photo or video of our trek please do not
+                        forget to send it to MWG or AAG on Whatsapp.
+                    """
+                     )
+            st.write("Enjoy!!")
+            st.write("To open or close side bar click on'>' or 'x'  on top left corner")
+
         
-                        Enjoy !!
-                    """
-                    )   
-            
             st.write("---")
             st.write(":rainbow[Website created and maintained by MWG and AAG]")
 
